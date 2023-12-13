f9ce5142b04427f7bdbf84ae13dcbd7335994c1b
elizabeth-poggie <elizabethpoggie@gmail.com>
2023-12-13 12:15:55 -0500

    boilerplate code

---
+++ _presentations/custom-slack-emojis.md
@@ -0,0 +1,27 @@
+---
+title: 'Custom Slack Emojis: Best Practices'
+excerpt: 'A community building workshop @ SSENSE'
+coverImage: '/assets/presentations/CustomSlackEmojis/cover.jpg'
+date: '2020-03-16T05:35:07.322Z'
+ogImage:
+  url: '/assets/presentations/CustomSlackEmojis/cover.jpg'
+---
+
+# Event Details
+
+Innovation Day @ SSENSE
+
+September 7th, 2022
+
+[Slides](/assets/presentations/CustomSlackEmojis/custom-slack-emojis.pdf)
+
+# Resources
+https://www.remove.bg/upload
+
+https://ezgif.com/help/adding-background-to-transparent-gif
+
+https://www.remove.bg/upload
+
+https://ezgif.com/maker
+
+https://makeemoji.com/
---
+++ pages/index.tsx
@@ -0,0 +1,43 @@
+
+import Layout from "../components/templates/layout";
+import { getAllPresentations } from "../lib/api";
+import Head from "next/head";
+import Post from "../interfaces/post";
+import Note from "../interfaces/note";
+import Presentation from "../interfaces/presentation";
+import Text from "../components/typography/Text/text";
+
+type Props = {
+  allPosts: Post[];
+  allNotes: Note[];
+  allPresentations: Presentation[];
+};
+
+
+export default function Index({ allPresentations }: Props) {
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>Elizabeth Poggie</title>
+        </Head>
+            <Text variant="h1">Elizabeth Poggie</Text>
+      </Layout>
+    </>
+  );
+}
+
+export const getStaticProps = async () => {
+
+  const allPresentations = getAllPresentations([
+    "title",
+    "date",
+    "slug",
+    "coverImage",
+    "excerpt",
+  ]);
+
+  return {
+    props: { allPresentations },
+  };
+};
