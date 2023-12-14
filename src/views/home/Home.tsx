5937ada951fc7fb174cd684e97536c0352a4eec5
elizabeth-poggie <elizabethpoggie@gmail.com>
2023-12-14 16:56:49 -0500

    blurb and text styling config

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -5,7 +5,7 @@ import { Image } from "../../components/display/image/image";
 
 // TODO - import good fonts
 // TODO - Draft Vision
-// TODO - fix colours
+// TODO - fix colors
 export function Home() {
   return (
     <div>
@@ -13,7 +13,13 @@ export function Home() {
         Elizabeth Poggie
       </Text>
       <Image src={cover.src} variant="cover" />
-      <Text variant="h1">Tech Enthusiast && Coordinator</Text>
+      <Text variant="h3" align="right">
+        My name is Elizabeth Poggie and I'm a Software Developer with a passion
+        for coordination. Since 2019, I've been merging my love of frontend
+        development with event planning by working on highly interdisciplinary
+        projects. In the right environment, I like to merge creativity in my
+        work by incorporating my own artistic touch.
+      </Text>
     </div>
   );
 }
