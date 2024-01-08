1bc03181cdb3a0026ca44139cd0f9a765e6b7a74
Poggie <42819849+elizabeth-poggie@users.noreply.github.com>
2024-01-08 11:28:44 -0500

    Fix Blurb Part 2 (#10)
    * yeet
    
    * yeet x2

--- src/views/about/about.tsx
+++ src/views/about/about.tsx
@@ -21,10 +21,11 @@ export function About() {
       <div className={styles.container}>
         <div className={styles.content}>
           <Text variant="p" align="justify">
-            Software developer with a passion for coaching. Since 2019, I've
-            been merging my love of development with mentoring by developing
-            interactive workshops. In the right environment, I like to blend
-            creativity in my work by incorporating my own artistic touch.
+            Software developer with a passion for coaching and coordination.
+            Since 2019, I've been merging my development skills with my passion
+            by working on highly interdisciplinary projects. In the right
+            environment, I like to blend creativity into my work by
+            incorporating my own artistic touch.
           </Text>
         </div>
       </div>
