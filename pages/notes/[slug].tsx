0a2ae0e49c62042d04cb9d352139cd564d297478
elizabeth-poggie <elizabethpoggie@gmail.com>
2024-02-21 11:03:44 -0500

    some fixes

--- pages/notes/[slug].tsx
+++ pages/notes/[slug].tsx
@@ -43,18 +43,30 @@ type Params = {
 export async function getStaticProps({ params }: Params) {
   const details = getBySlug(
     params.slug,
-    ["slug", "category", "type", "number", "title", "subtitle", "link"],
+    [
+      "slug",
+      "category",
+      "type",
+      "number",
+      "title",
+      "subtitle",
+      "link",
+      "content",
+    ],
     note2Directory
   );
+
+  console.log(details);
 
   // filtered by type and course
   const filteredNotes = getAllNotes([
     "slug",
     "category",
+    "type",
     "number",
     "title",
   ]).filter(
-    (note) => note.course === details.category && note.type === details.type
+    (note) => note.category === details.category && note.type === details.type
   );
 
   return {
