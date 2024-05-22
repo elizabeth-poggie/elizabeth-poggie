1195ef2154b026b4ed658bcaf09e0cb482b90452
Poggie <elizabethpoggie@gmail.com>
2024-05-22 18:52:43 -0400

    still is shit but going somewhere

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import styles from "../src/styles/app.module.scss";
 // @ts-ignore: Hacky work around to make my page not have visible scroll bars
 import noScrollStyles from "../src/styles/app.css";
+import { useMDXComponents } from "../src/mdx-components/useMDXComponents";
+import { MDXProvider } from "@mdx-js/react";
 export default function MyApp({ Component, pageProps }: AppProps) {
   /**
    * Read more about editing the body tag of Next.js applications here
@@ -11,5 +13,16 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   React.useEffect(() => {
     document.body.className = styles.layout;
   });
-  return <Component style={noScrollStyles} {...pageProps} />;
+
+  /**
+   * Read more about this logic here
+   * https://nextjs.org/docs/pages/building-your-application/configuring/mdx
+   *
+   */
+  const components = useMDXComponents({});
+  return (
+    <MDXProvider components={components}>
+      <Component style={noScrollStyles} {...pageProps} />
+    </MDXProvider>
+  );
 }
---
+++ src/mdx-components/useMDXComponents.tsx
@@ -0,0 +1,23 @@
+import type { MDXComponents } from "mdx/types";
+import {
+  MDHeader,
+  MDImage,
+  MDParagraph,
+  MDSubHeader,
+  MDUnorderedList,
+} from "../components/display/md-note-content/md-note-content";
+
+// This file allows you to provide custom React components
+// to be used in MDX files. You can import and use any
+// React component you want, including inline styles,
+// components from other libraries, and more.
+
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return {
+    h1: MDHeader,
+    h2: MDSubHeader,
+    p: MDParagraph,
+    ul: MDUnorderedList,
+    img: MDImage,
+  };
+}
