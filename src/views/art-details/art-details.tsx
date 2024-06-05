36fc7f4dab184f9c55711c8948c7952e21c909a5
Poggie <elizabethpoggie@gmail.com>
2024-06-05 08:30:18 -0400

    yeet

--- src/views/art-details/art-details.tsx
+++ src/views/art-details/art-details.tsx
@@ -1,15 +1,19 @@
 import { MDXProps } from "../../../pages/recipes/[slug]";
 import { Image, MDXImageWrapper } from "../../components/display/image/image";
-import { MDXNoteContent } from "../../components/display/mdx-note-content/mdx-note-content";
+import {
+  MDXImage,
+  MDXNoteContent,
+} from "../../components/display/mdx-note-content/mdx-note-content";
 
 export function ArtDetails(props: MDXProps) {
   const { coverSrc } = props.source.frontmatter;
   const baseSrc = props.baseSrc + coverSrc;
   console.log(baseSrc);
+  // will this work lmao
   return (
     <div>
       <MDXNoteContent {...props} />
-      <Image src={baseSrc} />
+      <MDXImage src={coverSrc} alt="yeet" />
     </div>
   );
 }
