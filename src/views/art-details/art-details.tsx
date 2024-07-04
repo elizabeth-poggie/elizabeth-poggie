d9936462c880a14f68124f67f0cbe7ef4025c2d1
Poggie <elizabethpoggie@gmail.com>
2024-07-04 19:13:33 -0400

    yeet

--- src/views/art-details/art-details.tsx
+++ src/views/art-details/art-details.tsx
@@ -3,15 +3,23 @@ import {
   MDXImage,
   MDXNoteContent,
 } from "../../components/display/mdx-note-content/mdx-note-content";
+import { Text } from "../../components/typography/text/text";
 import styles from "./art-details.module.scss";
 
 export function ArtDetails(props: MDXProps) {
-  const { coverSrc } = props.source.frontmatter;
+  const { coverSrc, title, subtitle, created } = props.source.frontmatter;
   const { baseFolder } = props;
   return (
-    <div>
-      <MDXNoteContent {...props} />
-      <div>
+    <div className={styles.container}>
+      <div className={styles.noteContainer}>
+        <div className={styles.noteTitle}>
+          <Text variant="h3">{created.split("-")[0]}</Text>
+        </div>
+        <Text variant="title">{title}</Text>
+        <Text variant="subheading2">{subtitle}</Text>
+        <MDXNoteContent {...props} />
+      </div>
+      <div className={styles.artContainer}>
         <MDXImage src={coverSrc} slug={baseFolder} />
       </div>
     </div>
