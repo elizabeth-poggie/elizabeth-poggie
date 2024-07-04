4167d9fb7430e383d810c9eb4fac375397374bd3
Poggie <elizabethpoggie@gmail.com>
2024-07-04 18:20:42 -0400

    init

--- src/views/art-details/art-details.tsx
+++ src/views/art-details/art-details.tsx
@@ -3,15 +3,17 @@ import {
   MDXImage,
   MDXNoteContent,
 } from "../../components/display/mdx-note-content/mdx-note-content";
+import styles from "./art-details.module.scss";
 
 export function ArtDetails(props: MDXProps) {
   const { coverSrc } = props.source.frontmatter;
   const { baseFolder } = props;
-
   return (
     <div>
       <MDXNoteContent {...props} />
-      <MDXImage src={coverSrc} slug={baseFolder} />
+      <div>
+        <MDXImage src={coverSrc} slug={baseFolder} />
+      </div>
     </div>
   );
 }
