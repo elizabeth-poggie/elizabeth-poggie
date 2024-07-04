8f2b461853befbbaa4d26a4d0f845d37aef751d5
Poggie <elizabethpoggie@gmail.com>
2024-07-04 17:11:00 -0400

    yeet

--- src/utils/helpers/mdxLoader.ts
+++
@@ -1,9 +0,0 @@
-import replaceAll from "string.prototype.replaceall";
-
-export default function mdxLoader(content: string): string {
-  return replaceAll(
-    content,
-    /\!\[(.*)\]\((.+)\)/g,
-    `<NextImage alt="$1" src={require('$2').default} />`
-  );
-}
