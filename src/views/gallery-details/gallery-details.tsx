c525d140a9eb8bfd4fe7511b7ff1faf0d98cd8e3
Poggie <elizabethpoggie@gmail.com>
2024-07-26 15:34:04 -0400

    soooooo much cleaner

--- src/views/gallery-details/gallery-details.tsx
+++ src/views/gallery-details/gallery-details.tsx
@@ -10,9 +10,17 @@ import styles from "./gallery-details.module.scss";
 export function GalleryDetails(props: MDXProps) {
   const { coverSrc, title, subtitle, created } = props.source.frontmatter;
   const { baseFolder } = props;
+  const year = created.split("-")[0];
   return (
     <div className={styles.container}>
       <div className={cs(styles.column, styles.artContainer)}>
+        <div className={styles.smallNoteTitle}>
+          <div className={styles.year}>
+            <Text variant="h3">{year}</Text>
+          </div>
+          <Text variant="title">{title}</Text>
+          <Text variant="subheading2">{subtitle}</Text>
+        </div>
         <MDXImage
           src={coverSrc}
           baseFolder={baseFolder}
@@ -21,11 +29,13 @@ export function GalleryDetails(props: MDXProps) {
       </div>
       <div className={styles.column}>
         <div className={styles.noteContainer}>
-          <div className={styles.noteTitle}>
-            <Text variant="h3">{created.split("-")[0]}</Text>
+          <div className={styles.bigNoteTitle}>
+            <div className={styles.year}>
+              <Text variant="h3">{year}</Text>
+            </div>
+            <Text variant="title">{title}</Text>
+            <Text variant="subheading2">{subtitle}</Text>
           </div>
-          <Text variant="title">{title}</Text>
-          <Text variant="subheading2">{subtitle}</Text>
           <MDXNoteContent {...props} />
         </div>
       </div>
