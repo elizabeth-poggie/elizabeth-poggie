f077758421410f1b7f372b0a889ff3618d1aeb10
Poggie <42819849+elizabeth-poggie@users.noreply.github.com>
2024-07-26 20:51:04 -0400

    Mobile Layouts (recipes look nicer now) (#67)
    * yeet
    
    * soooooo much cleaner
    
    * it werks
    
    * perf
    
    * yeet
    
    * yeet

--- src/views/gallery-details/gallery-details.tsx
+++ src/views/gallery-details/gallery-details.tsx
@@ -10,24 +10,34 @@ import styles from "./gallery-details.module.scss";
 export function GalleryDetails(props: MDXProps) {
   const { coverSrc, title, subtitle, created } = props.source.frontmatter;
   const { baseFolder } = props;
+  const year = created.split("-")[0];
   return (
     <div className={styles.container}>
-      <div className={styles.column}>
-        <div className={styles.noteContainer}>
-          <div className={styles.noteTitle}>
-            <Text variant="h3">{created.split("-")[0]}</Text>
+      <div className={cs(styles.column, styles.artContainer)}>
+        <div className={styles.smallNoteTitle}>
+          <div className={styles.year}>
+            <Text variant="h3">{year}</Text>
           </div>
           <Text variant="title">{title}</Text>
           <Text variant="subheading2">{subtitle}</Text>
-          <MDXNoteContent {...props} />
         </div>
-      </div>
-      <div className={cs(styles.column, styles.artContainer)}>
         <MDXImage
           src={coverSrc}
           baseFolder={baseFolder}
           variant="galleryDetailsItem"
         />
+      </div>
+      <div className={styles.column}>
+        <div className={styles.noteContainer}>
+          <div className={styles.bigNoteTitle}>
+            <div className={styles.year}>
+              <Text variant="h3">{year}</Text>
+            </div>
+            <Text variant="title">{title}</Text>
+            <Text variant="subheading2">{subtitle}</Text>
+          </div>
+          <MDXNoteContent {...props} />
+        </div>
       </div>
     </div>
   );
