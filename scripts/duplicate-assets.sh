2763a89c2d84fd2e4498ff4bcca57d8958fa8d8d
Poggie <elizabethpoggie@gmail.com>
2024-07-27 11:50:28 -0400

    cute script

--- scripts/duplicate-assets.sh
+++ scripts/duplicate-assets.sh
@@ -16,10 +16,31 @@ duplicate_all_assets() {
 
         # Copy all files from the source assets directory to the destination directory
         cp -r "$assets_dir/"* "$dest_dir"
-
-        echo "Copied assets from $assets_dir to $dest_dir"
     done
 }
+
+
+# Define color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+NC='\033[0m' # No Color
+
+echo "📠  Let's get snoopy to copy and paste that for you... "
+echo " "
+echo -e "${WHITE}  ,-~~-.___.${NC}"
+echo -e "${WHITE} / |  '     \\         ${GREEN}beep boop beep boop${NC}"
+echo -e "${WHITE}(  )         0${NC}"              
+echo -e "${WHITE} \\_/-, ,----'${NC}"            
+echo -e "${RED}    ====           ${WHITE}//${NC}"                     
+echo -e "${WHITE}   /  \\-'~;    ${YELLOW}/${WHITE}~~~${YELLOW}(${WHITE}O${YELLOW})${NC}"
+echo -e "${WHITE}  /  __/~|   ${YELLOW}/       |${NC}"     
+echo -e "${WHITE}=(  _____| ${YELLOW}(_________|${NC}"
+echo " "
 
 # Call the function to duplicate all assets
 duplicate_all_assets
