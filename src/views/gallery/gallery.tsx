b2bbfd618f9f182832039e89d57d0a060a56d5fc
Poggie <elizabethpoggie@gmail.com>
2024-07-29 14:57:19 -0400

    sort by created descending

--- src/views/gallery/gallery.tsx
+++ src/views/gallery/gallery.tsx
@@ -2,6 +2,7 @@ import { MDXImage } from "../../components/display/mdx-note-content/mdx-note-con
 import { Link } from "../../components/navigation/link/link";
 import { Text } from "../../components/typography/text/text";
 import { INote } from "../../interfaces/note";
+import { sortByCreatedDescending } from "../../utils/helpers/noteSorters";
 import styles from "./gallery.module.scss";
 
 interface IProps {
@@ -9,9 +10,10 @@ interface IProps {
 }
 
 export function Gallery({ allNotes }: IProps) {
+  const sortedNotes = sortByCreatedDescending(allNotes);
   return (
     <div className={styles.container}>
-      {allNotes.map((note: INote) => {
+      {sortedNotes.map((note: INote) => {
         return (
           <>
             <Link href={note.slug}>
