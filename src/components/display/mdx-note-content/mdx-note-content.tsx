40de4b6a77aa8150b949dd3fab62a3272a2d05cc
Poggie <elizabethpoggie@gmail.com>
2024-08-01 04:17:17 -0400

    Syntax highlighting

--- src/components/display/mdx-note-content/mdx-note-content.tsx
+++ src/components/display/mdx-note-content/mdx-note-content.tsx
@@ -8,6 +8,9 @@ import {
   MDUnorderedList,
 } from "../md-note-content/md-note-content";
 import { IImageProps, Image } from "../image/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import styles from "./mdx-note-content.module.scss";
 
 export function MDXNoteContent({ source, baseFolder }: MDXProps) {
   return (
@@ -20,6 +23,8 @@ export function MDXNoteContent({ source, baseFolder }: MDXProps) {
         ul: MDUnorderedList,
         a: ({ children, href }) => MDLink({ children, href, baseFolder }),
         img: (props) => <MDXImage {...props} baseFolder={baseFolder} />,
+        code: ({ className, children, ...props }) =>
+          MdxCode({ className, children, ...props }),
       }}
     />
   );
@@ -47,5 +52,25 @@ export const MDXImage = ({
       src={src}
       alt={alt}
     />
+  );
+};
+
+export const MdxCode = ({ className, children, ...props }) => {
+  const match = /language-(\w+)/.exec(className || "");
+  return match ? (
+    <div className={styles.codeBlock}>
+      <SyntaxHighlighter
+        style={atomDark}
+        language={match[1]}
+        PreTag="div"
+        {...props}
+      >
+        {String(children).replace(/\n$/, "")}
+      </SyntaxHighlighter>
+    </div>
+  ) : (
+    <span className={styles.inlineCode} {...props}>
+      {children}
+    </span>
   );
 };
