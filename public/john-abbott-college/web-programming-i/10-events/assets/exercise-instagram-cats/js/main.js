e15be26a4c10f1abad5459459c4d327fdd876a6a
Poggie <elizabethpoggie@gmail.com>
2024-09-13 10:12:46 -0400

    yeet

---
+++ public/john-abbott-college/web-programming-i/10-events/assets/exercise-instagram-cats/js/main.js
@@ -0,0 +1,21 @@
+// Exercise 1 - Create an event listener such that it opens up an "alert" each time a user clicks the camera. Make sure to put the right cat name in the alert message
+
+// Defining constants you need
+const camera1 = document.getElementById("camera1");
+const camera2 = document.getElementById("camera2");
+const camera3 = document.getElementById("camera3");
+const camera4 = document.getElementById("camera4");
+
+// Put Logic here
+// @TODO
+
+// Exercise 2 - Create an event listener such that the profile changes to a new image of your choice when the user hovers over their avatar and reverts back when they are no longer hovering
+
+// Defining constants you need
+const profile1 = document.getElementById("profile1");
+const profile2 = document.getElementById("profile2");
+const profile3 = document.getElementById("profile3");
+const profile4 = document.getElementById("profile4");
+
+// Put Logic here
+// @TODO
