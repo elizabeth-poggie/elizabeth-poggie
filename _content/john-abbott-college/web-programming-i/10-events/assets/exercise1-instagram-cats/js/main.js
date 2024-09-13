d00ec8411dd1dbd068b30c6983abbb336e24a599
Poggie <elizabethpoggie@gmail.com>
2024-09-13 08:40:19 -0400

    yeet

---
+++ _content/john-abbott-college/web-programming-i/10-events/assets/exercise1-instagram-cats/js/main.js
@@ -0,0 +1,7 @@
+// Defining constants
+const camera1 = document.getElementById("camera1");
+const camera2 = document.getElementById("camera2");
+const camera3 = document.getElementById("camera3");
+const camera4 = document.getElementById("camera4");
+
+// Exercise 1 - Create an event listener such that it opens up an "alert" each time a user clicks the camera. Make sure to put the right cat name in the alert message
