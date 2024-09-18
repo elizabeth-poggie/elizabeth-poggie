216eb08fb524c52b405c11ebc158101e753c10d8
Poggie <elizabethpoggie@gmail.com>
2024-09-18 14:22:38 -0400

    notesssss

---
+++ src/components/templates/three-collumn/three-collumn.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./three-collumn.module.scss";
+
+interface IProps {
+  header: React.ReactNode;
+  rightSidebar: React.ReactNode;
+  mainContent: React.ReactNode;
+  leftSidebar: React.ReactNode;
+}
+
+export function ThreeColumnTemplate({
+  header,
+  rightSidebar,
+  mainContent,
+  leftSidebar,
+}: IProps) {
+  return (
+    <>
+      <div className={styles.container}>
+        {header}
+        <main className={styles.main}>
+          <section className={styles.sideBarSection}>{leftSidebar}</section>
+          <section className={styles.contentSection}>{mainContent}</section>
+          <section className={styles.sideBarSection}>{rightSidebar}</section>
+        </main>
+      </div>
+    </>
+  );
+}
