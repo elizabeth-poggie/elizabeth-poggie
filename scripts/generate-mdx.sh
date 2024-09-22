f5cb9577c960b3b2c058d5960a29e32541439e69
Poggie <42819849+elizabeth-poggie@users.noreply.github.com>
2024-09-22 08:51:44 -0400

    1W5 & 1B4 Week 4 (#91)
    * move doc
    
    * title changes
    
    * it werks
    
    * yeeeeeeeeet
    
    * yeeee mdx generation fixes
    
    * yeet imports
    
    * yeet
    
    * yeet
    
    * yeet
    
    * demo boilerplate
    
    * yeet
    
    * yeet
    
    * yeet
    
    * solution improvements
    
    * yeet demo
    
    * yeet
    
    * yeet
    
    * yeet
    
    * diagrams are important
    
    * yeet
    
    * more diagrams and typos
    
    * yeet
    
    * moving things for now

--- scripts/generate-mdx.sh
+++ scripts/generate-mdx.sh
@@ -5,7 +5,7 @@ print_menu() {
     local options=("$@")
     for i in "${!options[@]}"; do
         if [ $i -eq $selected ]; then
-            printf "\e[1;32m> ${options[$i]}\e[0m\n"  # Highlight selected option
+            printf "\e[1;32m> ${options[$i]}\e[0m\n" 
         else
             printf "  ${options[$i]}\n"
         fi
@@ -16,8 +16,6 @@ print_menu() {
 capture_selection() {
     local options=("$@")
     selected=0
-
-    # Hide the cursor
     tput civis
 
     while true; do
@@ -42,9 +40,7 @@ capture_selection() {
         fi
     done
 
-    # Show the cursor
     tput cnorm
-
     return $selected
 }
 
@@ -76,6 +72,34 @@ echo "Enter category: "
 capture_selection "${categories[@]}"
 category="${categories[$?]}"
 
+# Define sub categories based on category selection
+subcategories=()
+if [ "$directory" = "john-abbott-college" ]; then
+    case $category in
+        "Web Programming I")
+            subcategories=("lectures" "assignments" "quiz-answers")
+            ;;
+        "Computerized Systems")
+            subcategories=("lectures" "assignments")
+            ;;
+        "Admin")
+            subcategories=("instructor-info" "schedules")
+            ;;
+        "User Interfaces")
+            subcategories=("lectures" "assignments")
+            ;;
+        *)
+            echo "No subcategories available for this category."
+            exit 1
+            ;;
+    esac
+
+    # Prompt the user to select a subcategory
+    echo "Select a sub category: "
+    capture_selection "${subcategories[@]}"
+    subcategory="${subcategories[$?]}"
+fi
+
 # Prompt the user to enter a title
 echo "Enter title: "
 read title
@@ -86,11 +110,21 @@ created=$(date +'%Y-%m-%d')
 # Define the file name based on the title (replace spaces with hyphens and make lowercase)
 file_name=$(echo "$title" | tr ' ' '-' | tr '[:upper:]' '[:lower:]')
 kebabed_category=$(echo "$category" | tr ' ' '-' | tr '[:upper:]' '[:lower:]')
+# Create target directories based on the directory, category, and subfolder (if applicable)
+if [ "$directory" = "john-abbott-college" ]; then
+    kebabed_sub_category=$(echo "$subcategory" | tr ' ' '-' | tr '[:upper:]' '[:lower:]')
+    target_dir="_content/$directory/$kebabed_category/$kebabed_sub_category"
+else
+    target_dir="_content/$directory/$kebabed_category"
+fi
 
-# target directories must exist
-target_dir="_content/$directory/$kebabed_category/$file_name"
-mkdir -p "$target_dir"
-mkdir -p "$target_dir/assets" # also an assets folder is nice
+# Count existing files in the directory to assign the next number
+file_count=$(ls "$target_dir" | grep -E '^[0-9]+-' | wc -l)
+next_number_unformatted=$((file_count + 1))
+next_number=$(printf "%02d" $next_number_unformatted)
+
+# Append the number to the file name
+file_name="$next_number-$file_name"
 
 # Define the content of the MDX file
 content="---
@@ -98,9 +132,14 @@ content="---
 category: \"$category\"
 title: \"$title\"
 created: \"$created\"
+number: $next_number_unformatted
 ---"
 
-# Write the content to the MDX file
-echo -e "$content" > "$target_dir/$file_name.mdx"
+# Create the target directories
+mkdir -p "$target_dir/$file_name"
+mkdir -p "$target_dir/$file_name/assets"
 
-echo "MDX file generated: $target_dir/$file_name.mdx"
+# Write the content to the MDX file
+echo -e "$content" > "$target_dir/$file_name/$file_name.mdx"
+
+echo "MDX file generated: $target_dir/$file_name/$file_name.mdx"
