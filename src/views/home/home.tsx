4c83a75dac66de57828c598c89053d3bca628fbb
Poggie <42819849+elizabeth-poggie@users.noreply.github.com>
2024-10-02 11:08:42 -0400

    Fix Layouts for smaller laptops and improve entry point lol (#95)
    * yeet
    
    * better but still not good
    
    * better
    
    * clean
    
    * clean
    
    * yeet
    
    * go0od enough
    
    * text improvements
    
    * make it cuter tho

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,52 +1,39 @@
+import { navItems } from "../../../pages";
 import { CollectionItemLink } from "../../components/navigation/collection-item-link/collection-item-link";
 import { TextLink } from "../../components/navigation/link/link";
 import { Text } from "../../components/typography/text/text";
 import { INote } from "../../interfaces/note";
 import styles from "./home.module.scss";
 
-// TODO - Just import my README.md it's SOOOOO much easier lol (1 place to maintain it)
 export function Home() {
   return (
     <div className={styles.container}>
       <section className={styles.section}>
-        <Text variant="title" align="center">
-          About
+        <Text variant="title" align="center" gutterBottom={4}>
+          Elizabeth Poggie
         </Text>
-        <div className={styles.paragraphContainer}>
-          <Text variant="p">
-            I am currently a lecturer @ John Abbott College, the community
-            manager @ Marché Sainte-Anne, and a freelance artist.
+        <nav className={styles.navigation}>
+          {navItems.map((item, index) => {
+            return (
+              <>
+                <div className={styles.navItemWrapper}>
+                  <TextLink href={item.href} variant="h2">
+                    {item.text}
+                  </TextLink>
+                </div>
+                {index < navItems.length - 1 && (
+                  <span className={styles.dotWrapper}> • </span>
+                )}
+              </>
+            );
+          })}
+        </nav>
+        <div className={styles.coolText}>
+          <Text variant="p" align="center">
+            I’m currently a lecturer at John Abbott College and also manage
+            various events throughout Montreal.
           </Text>
         </div>
-        <div className={styles.paragraphContainer}>
-          <Text variant="p">
-            Since 2019, I've been merging my technical expertise with my love of
-            problem-solving by working on highly interdisciplinary projects. In
-            the right environment, I like to blend creativity into my work by
-            adding my own artistic touch.
-          </Text>
-        </div>
-        <div className={styles.paragraphContainer}>
-          <Text variant="p">
-            When I am not hidden away in the corners of Penfield, you'll often
-            spot me at a farmers market, creating another strange piece of art
-            or biting off more work than I can chew (again).
-          </Text>
-        </div>
-      </section>
-      <section className={styles.section}>
-        <Text variant="title" align="center">
-          Contact
-        </Text>
-        <TextLink
-          variant="p"
-          align="center"
-          href="mailto:elizabethpoggie@gmail.com"
-          decoration="underline"
-          color="green"
-        >
-          elizabethpoggie@gmail.com
-        </TextLink>
       </section>
     </div>
   );
