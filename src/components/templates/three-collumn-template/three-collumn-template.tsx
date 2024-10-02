fc56fe8b0089c5b2d80a31800ce0e63fda52a0f4
Poggie <elizabethpoggie@gmail.com>
2024-10-02 11:06:35 -0400

    make it cuter tho

--- src/components/templates/three-collumn-template/three-collumn-template.tsx
+++ src/components/templates/three-collumn-template/three-collumn-template.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./three-collumn-template.module.scss";
+import { cs } from "../../../utils/helpers/classHelpers";
 
 interface IProps {
   header: React.ReactNode;
@@ -19,7 +20,9 @@ export function ThreeColumnTemplate({
       <div className={styles.container}>
         <header className={styles.header}>{header}</header>
         <main className={styles.main}>
-          <section className={styles.sideBarSection}>{leftSidebar}</section>
+          <section className={cs(styles.sideBarSection, styles.left)}>
+            {leftSidebar}
+          </section>
           <section className={styles.contentSection}>{mainContent}</section>
           <section className={styles.sideBarSection}>{rightSidebar}</section>
         </main>
