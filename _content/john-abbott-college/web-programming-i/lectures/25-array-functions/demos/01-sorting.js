29acd6ce60dc499e00e5fff40883d90ff04e51ed
Poggie <elizabethpoggie@gmail.com>
2024-10-22 09:08:58 -0400

    better notes

---
+++ _content/john-abbott-college/web-programming-i/lectures/25-array-functions/demos/01-sorting.js
@@ -0,0 +1,8 @@
+let numbers = [12, 5, 20, 1];
+// Ascending order
+numbers.sort((a, b) => a - b);
+console.log(numbers);
+
+// Descending order
+numbers.sort((a, b) => b - a);
+console.log(numbers);
