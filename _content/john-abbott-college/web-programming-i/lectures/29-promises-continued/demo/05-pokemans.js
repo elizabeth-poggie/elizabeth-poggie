5d71e97c6158ea8aa8a1c6d81a75bfedca968f49
Poggie <elizabethpoggie@gmail.com>
2024-11-01 12:06:49 -0400

    remove

--- _content/john-abbott-college/web-programming-i/lectures/29-promises-continued/demo/05-pokemans.js
+++ _content/john-abbott-college/web-programming-i/lectures/29-promises-continued/demo/05-pokemans.js
@@ -3,16 +3,3 @@ const urls = [
   "https://pokeapi.co/api/v2/pokemon/pikachu",
   "https://pokeapi.co/api/v2/pokemon/squirtle",
 ];
-
-const pokemons = [];
-
-const pokemonLoop = async () => {
-  for await (const url of urls) {
-    const pokemon = await fetch(url);
-    pokemons.push(pokemon);
-  }
-};
-
-await pokemonLoop();
-
-console.log(pokemons);
