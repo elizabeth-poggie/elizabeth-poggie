9fb344cbf53ac3d8cee5e5a11c9ac50f95b3425d
Poggie <elizabethpoggie@gmail.com>
2024-11-16 13:25:59 -0500

    yeet

--- _content/john-abbott-college/web-programming-i/lectures/34-test-2-review-game/demo/test.js
+++ _content/john-abbott-college/web-programming-i/lectures/34-test-2-review-game/demo/test.js
@@ -1,3 +1,14 @@
-let test = async function (x) {
-  // do cool async thing
-};
+function test() {
+  return new Promise((resolve, reject) => {
+    console.log("heya");
+    setTimeout(() => {
+      console.log("hi");
+      resolve(42);
+    });
+  });
+}
+console.log("Hola");
+let jimmy = test();
+console.log("yo");
+jimmy.then((bob) => console.log(bob));
+console.log("hey");
